fcb2d3850daf7f30732a19dcdda7721a34d17198
Pablo Gallina <pablogallinag@gmail.com>
2025-08-15 11:32:36 -0600

    add basic restfull api

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const person = {
+    name: "Pablo Gallina",
+    age: new Date().getFullYear() - 2000,
+    occupation: "Software Engineer",
+  };
+
+  return new Response(JSON.stringify(person), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
