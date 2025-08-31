5e3be7e76967b444190f8697ebd2a682bc17d823
Pablo Gallina <pablogallinag@gmail.com>
2025-08-31 13:46:25 -0600

    add client crud

--- src/pages/api/clients/[clientID].ts
+++ src/pages/api/clients/[clientID].ts
@@ -1,56 +1,94 @@
 import type { APIRoute } from "astro";
+import { Clients, db, eq } from "astro:db";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
-  const { clientID } = params;
+  try {
+    const { clientID } = params;
 
-  // if (!post)
-  //   return new Response(JSON.stringify({ error: "Post not found" }), {
-  //     status: 404,
-  //     headers: { "Content-Type": "application/json" },
-  //   });
+    const client = await db
+      .select()
+      .from(Clients)
+      .where(eq(Clients.clientID, +clientID!))
+      .get();
 
-  return new Response(JSON.stringify({ clientID }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    if (!client)
+      return new Response(JSON.stringify({ error: "Client not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+
+    return new Response(JSON.stringify(client), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to fetch client" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
 
 export const PUT: APIRoute = async ({ params, request }) => {
-  const { clientID } = params;
-  const body = await request.json();
+  try {
+    const { clientID } = params;
+    const body = await request.json();
 
-  // For demonstration purposes, we will not implement PUT functionality.
-  // In a real application, you would handle updating a post here.
+    await db.update(Clients).set(body).where(eq(Clients.clientID, +clientID!));
 
-  return new Response(JSON.stringify({ ...body, clientID }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    return new Response(JSON.stringify({ ...body, clientID }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to update client" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
 
 export const PATCH: APIRoute = async ({ params, request }) => {
-  const { clientID } = params;
+  try {
+    const { clientID } = params;
+    const body = await request.json();
 
-  // For demonstration purposes, we will not implement PATCH functionality.
-  // In a real application, you would handle partial updates to a post here.
-  const body = await request.json();
+    await db.update(Clients).set(body).where(eq(Clients.clientID, +clientID!));
 
-  return new Response(JSON.stringify({ ...body, clientID }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    return new Response(JSON.stringify({ ...body, clientID }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to update client" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
 
 export const DELETE: APIRoute = async ({ params, request }) => {
-  const { clientID } = params;
+  try {
+    const { clientID } = params;
 
-  // For demonstration purposes, we will not implement DELETE functionality.
-  // In a real application, you would handle deleting a post here.
+    await db
+      .update(Clients)
+      .set({ isActive: false })
+      .where(eq(Clients.clientID, +clientID!));
 
-  return new Response(JSON.stringify({ clientID }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    return new Response(
+      JSON.stringify({ message: "Client deleted successfully" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to delete client" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,18 +1,50 @@
 import type { APIRoute } from "astro";
+import { Clients, db, eq } from "astro:db";
 export const prerender = false;
+export const GET: APIRoute = async ({ request }) => {
+  try {
+    const url = new URL(request.url);
+    const isActive = url.searchParams.get("isActive");
 
-export const GET: APIRoute = async ({ params, request }) => {
-  return new Response(JSON.stringify([]), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    const clients = await db
+      .select()
+      .from(Clients)
+      .where(
+        isActive !== null
+          ? eq(Clients.isActive, isActive === "true")
+          : undefined
+      )
+      .all();
+
+    return new Response(JSON.stringify(clients), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch {
+    return new Response(JSON.stringify({ error: "Failed to fetch clients" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
 
 export const POST: APIRoute = async ({ request }) => {
-  const body = await request.json();
+  try {
+    const body = await request.json();
 
-  return new Response(JSON.stringify(body), {
-    status: 201,
-    headers: { "Content-Type": "application/json" },
-  });
+    const newClient = await db.insert(Clients).values(body);
+    const clientID = +newClient?.lastInsertRowid?.toString()!;
+
+    return new Response(JSON.stringify({ clientID, ...body }), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.log("Error creating client:", error);
+
+    return new Response(JSON.stringify({ error: "Failed to create client" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
